9d007f132f5272d64857cc9cf1d27c7318e4a02f
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-18 13:02:49 +0300

    added angular props

--- apps/ng-story-exec/src/app/app.component.ts
+++ apps/ng-story-exec/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'storybook-props-mismatch-root',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-story-exec';
+  @Input() title: string;
+  @Input() rating: number;
+  @Input() flag: boolean;
 }
