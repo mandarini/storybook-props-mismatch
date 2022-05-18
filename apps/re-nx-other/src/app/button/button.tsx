73a05885c65ff45aabd7f64ac9038f878c4edc68
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-18 17:35:42 +0300

    new component with no props more complicated

---
+++ apps/re-nx-other/src/app/button/button.tsx
@@ -0,0 +1,18 @@
+interface PropsWithClassName {
+  className?: string;
+}
+
+export interface ButtonProps extends PropsWithClassName {
+  disabled?: boolean;
+  primary?: boolean;
+  onClick?(event: React.MouseEvent): void;
+}
+
+export function Button(props: React.PropsWithChildren<ButtonProps>) {
+  const { disabled, onClick, children } = props;
+  return (
+    <button disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
