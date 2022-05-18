098c1cfc07c7fba23d1a8e494d38db8c974844d7
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-05-18 13:09:50 +0300

    generating storybook

---
+++ apps/ng-nx-exec-e2e/src/integration/app.component.spec.ts
@@ -0,0 +1,6 @@
+describe('ng-nx-exec', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary&args=title;rating;flag;'));
+  it('should render the component', () => {
+    cy.get('storybook-props-mismatch-root').should('exist');
+  });
+});
---
+++ apps/ng-story-exec-e2e/src/integration/app.component.spec.ts
@@ -0,0 +1,6 @@
+describe('ng-story-exec', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary&args=title;rating;flag;'));
+  it('should render the component', () => {
+    cy.get('storybook-props-mismatch-root').should('exist');
+  });
+});
